f6db990206a26158c567a26790b1215ce1ae9eb5
jaba <jaba@geometry.ooo>
2024-10-27 15:15:31 +0300

    init commit

---
+++ lib/avl_tree.ex
@@ -0,0 +1,18 @@
+defmodule AvlTree do
+  @moduledoc """
+  Documentation for `AvlTree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AvlTree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/avl_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule AvlTreeTest do
+  use ExUnit.Case
+  doctest AvlTree
+
+  test "greets the world" do
+    assert AvlTree.hello() == :world
+  end
+end
